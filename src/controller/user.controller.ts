a4e32bc3e14f9a1e7349975410ddb1fb3de25937
Aylin <aylin_santacruz@hotmail.com>
2024-11-06 17:27:52 -0500

    feat:Add getList and putUser

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,16 +1,36 @@
 import { Request, Response } from 'express';
-import { createUser } from '../services/user.service';
+import { allUserService, updateUserService } from '../services/user.service';
 import { IUser } from '../interface/index.interface';
 
 
-export const createUserController = async (req: Request, res: Response) => {
+export const getAllUser = async (req: Request, res: Response) => {
+    try {
+        const userAll = await allUserService();
+        res.status(200).json(userAll)
+    } catch (error: unknown) {
+        res.status(500).json({ message: 'An unexpected error has occurred' });
+    }
+
+}
+
+export const updateUserController = async (req: Request, res: Response) => {
+    const { user_id } = req.params;
+    const id = Number(user_id);
+    if (isNaN(id)) {
+        res.status(400).json({ message: 'Invalid user ID' }); 
+        return;
+    }
     const userData: IUser = req.body;
     try {
+        const updatedUser = await updateUserService(id, userData);
 
-        const newUser = await createUser(userData);
-        res.status(201).json(newUser);
-    } catch (error) {
-        console.error("Error al crear el usuario:", error);
-        res.status(500).json(error);
+        res.status(200).json(updatedUser);
+
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(404).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: 'An unexpected error has occurred' });
+        }
     }
-};
+}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,21 @@
 import { User } from "../models";
 import { IUser } from '../interface/index.interface';
 
-export const createUser = async (userData: IUser):Promise<IUser> => {
-        const newUser = await User.create(userData);
-        return newUser;
-};
+// export const createUser = async (userData: IUser):Promise<IUser> => {
+//         const newUser = await User.create(userData);
+//         return newUser;
+// };
+
+export const allUserService = async (): Promise<IUser[]> => {
+        const allUser = await User.findAll();
+        return allUser
+}
+
+export const updateUserService = async (id: number, data: IUser): Promise<IUser | null> => {
+        const user = await User.findByPk(id);
+        if (!user) {
+                throw new Error('the user does not exist')
+        }
+        const updatedUser = await user.update(data);
+        return updatedUser;
+}
