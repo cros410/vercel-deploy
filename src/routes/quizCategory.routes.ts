613ab18f45d611c3c04466f2561ee0d0fa9b8ad5
Antone Gonzales <es.angelika@gmail.com>
2024-11-09 00:10:55 -0500

    fix: add params to url

--- src/routes/quizCategory.routes.ts
+++ src/routes/quizCategory.routes.ts
@@ -1,13 +1,15 @@
 import express, { Router } from 'express';
-import { createQuizScore, getQuizQuestions } from '../controller/quizCategory.controller';
+import { createQuizScore, finishQuiz, getAllQuizCategories, getQuizQuestions } from '../controller/quizCategory.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
-
-
 const quizCategoryRouter: Router = express.Router();
 
 quizCategoryRouter.post('/quiz-score',createQuizScore);
-// quizCategoryRouter.post('/finish-quiz', finishQuiz);
+
+quizCategoryRouter.get('/quiz-categories', getAllQuizCategories);
 quizCategoryRouter.get('/quiz/:type/:type_id', isAuthenticated ,getQuizQuestions);
+quizCategoryRouter.post('/finish-quiz/:type/:type_id',isAuthenticated, finishQuiz);
+
+
 // quizCategoryRouter.put('/quiz-score', updateQuizScore);
 
 export {quizCategoryRouter}
