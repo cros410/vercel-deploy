9659bc6f14d5158a71e91a84f3715d8150bebce7
Antone Gonzales <es.angelika@gmail.com>
2024-11-08 21:07:44 -0500

    feat: add type assertion for custom request properties in auth middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -6,6 +6,7 @@ import { DecodedToken } from '../interface/index.interface';
 export const isAuthenticated = (req: Request, resp: Response, next: NextFunction): void => {
     try {
         const secretKey = process.env.JWT_SECRET || "secreto";
+        // console.log("🚀 ~ Clave secreta:", secretKey);
         const { authorization } = req.headers;
         if (!authorization) {
             resp.status(401).json({ message: "Authorization header is missing" });
@@ -13,24 +14,28 @@ export const isAuthenticated = (req: Request, resp: Response, next: NextFunction
         }
 
         const [type, token] = authorization.split(" ");
-        console.log("🚀 ~ authMiddleware ~ token:", token)
+        // console.log("🚀 ~ Tipo de token:", type);
+        // console.log("🚀 ~ Token recibido:", token);
+        // console.log("🚀 ~ authMiddleware ~ token:", token)
 
         if (type.toLowerCase() !== "bearer" || !token) {
             resp.status(401).json({ message: "Invalid authorization format" });
             return;
         }
         jwt.verify(token, secretKey, (err, decoded) => {
+            
             if (err) {
-                console.log(err);
                 resp.status(401).json({ message: "error del token" });
                 return;
             }
 
             const decodedToken = decoded as DecodedToken;
 
-            req.user_id = decodedToken.user_id;
-            req.username = decodedToken.username;
-            console.log("🚀 ~ jwt.verify ~ decodedToken:", decodedToken)
+             // Usar un "type assertion" para extender req temporalmente
+             (req as Request & { user_id: number; username: string }).user_id = decodedToken.user_id;
+             (req as Request & { user_id: number; username: string }).username = decodedToken.username;
+ 
+            // console.log("🚀 ~ jwt.verify ~ decodedToken:", decodedToken)
             
             next();
         });
